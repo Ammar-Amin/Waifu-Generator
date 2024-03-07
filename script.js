6a2b691ee2cfc5e14c42396429cd850105e2b616
Ammar-Amin <ammaramin5253@gmail.com>
2024-03-07 19:29:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let btn = document.querySelector('button');
+let img = document.querySelector('img');
+let h5 = document.querySelector('h5');
+
+
+// let imgDiv = document.querySelector('#img');
+// let img = document.createElement('img');
+
+btn.addEventListener('click', async () => {
+    try {
+        h5.innerHTML = "Loading Image...";
+        h5.style.textAlign = "center";
+
+        setTimeout(() => {
+            // h5.style.visibility = "hidden";
+            h5.innerHTML = ''
+        }, 2000)
+
+
+        let response = await fetch('https://api.waifu.pics/sfw/waifu')
+        let resData = await response.json();
+        // console.log(resData);
+
+        img.src = resData.url;
+        // imgDiv.appendChild(img);
+    } catch (error) {
+        h5.style.color = "white";
+        h5.style.textAlign = "center";
+        h5.innerHTML = error;
+    }
+})
